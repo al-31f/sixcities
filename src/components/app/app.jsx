5918e50f80a069f5e202241bb0ed89aa5e9a5180
Victor Polyakov <al-31f@yandex.ru>
2023-11-13 15:00:26 +0300

    Создает страницы приложения и настраивает маршрутизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import MainPage from '../main-page/main-page';
+import LoginPage from '../login-page/login-page';
+import FavoritesPage from '../favorites-page/favorites-page';
+import RoomPage from '../room-page/room-page';
+import NotFoundPage from '../not-found-page/not-found-page';
 
 const App = (props) => {
   const {sum, cards} = props;
 
   return (
-    <MainPage sum={sum}
-      cards={cards}/>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <MainPage sum={sum} cards={cards} />
+        </Route>
+        <Route path="/login" exact component={LoginPage} />
+        <Route path="/favorites" exact component={FavoritesPage} />
+        <Route path="/offer/:id?" exact component={RoomPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
